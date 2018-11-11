c71ae585c7432411751a3e9637d9de04a4590820
Anant Kaushik <anant.kaushik2@gmail.com>
2018-11-11 18:50:45 +0530

    Updated

--- setting.py
+++ setting.py
@@ -84,7 +84,8 @@ def recruiters():
 def events():
    con = sql.connect("static/test.db")
    cur = con.cursor()
-   cur.execute("select companyname,role,date,salary,venue,sno,salary,interviewprocess,other,link from event")
+   cur.execute("DELETE from event where date < (select date('now'))")
+   cur.execute("select companyname,role,date,salary,venue,sno,salary,interviewprocess,other,link from event ORDER BY date")
    inf = cur.fetchall()
    inf = [i for i in inf]
    for i in inf:
